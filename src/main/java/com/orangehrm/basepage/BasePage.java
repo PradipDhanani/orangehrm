ea848eead656f844821964aeef97eb8dfa0fad4a
PradipDhanani <pradip.dhanani@gmail.com>
2020-04-08 11:35:42 +0100

    orangehrm

---
+++ src/main/java/com/orangehrm/basepage/BasePage.java
@@ -0,0 +1,11 @@
+package com.orangehrm.basepage;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Pradip
+ */
+public class BasePage {
+    //web driver declare
+    public static WebDriver driver;
+}
